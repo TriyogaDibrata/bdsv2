21446df3b5628edd8b288865b6f00a26ddf7a7ff
triyogadibrata <triyoga.dibrata@gmail.com>
2023-08-29 13:19:09 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bds-v2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
